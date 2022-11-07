968624ee3cc74818ab2b88885e14244c80404665
Плохотнюк Артем <aplokhotnyuk@diasoft.ru>
2022-11-07 11:54:32 +0300

    HashPassword modify

--- session/user.go
+++ session/user.go
@@ -43,18 +43,23 @@ func UserRm(id uint64) {
 	sql().Delete(User{Id: id})
 }
 func UserCreate(usr User) {
+	usr.Password = HashPassword(usr.Password)
 	sql().Create(&usr)
 }
 func UserModify(usr User) {
+	logger.Println(usr)
 	var oldUsr User
 	sql().First(&oldUsr, User{Id: usr.Id})
 	if usr.Password == "" {
 		usr.Password = oldUsr.Password
+	} else {
+		usr.Password = HashPassword(usr.Password)
 	}
 	if usr.Access == 0 {
 		usr.Access = oldUsr.Access
 	}
 	sql().Save(&usr)
+	logger.Println(usr)
 }
 
 func GetUserById(id uint64) User {
